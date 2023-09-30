2a03fb636a96c5c3de216206f16f8112282a0aa1
Ishaan Bedi <bediishaan6@gmail.com>
2023-09-30 07:55:28 +0530

    Create skeleton for Hero Section & Add Sample Data

---
+++ lib/data.ts
@@ -0,0 +1,16 @@
+const user_data = {
+  name: "John Doe",
+  profile_image: "https://randomuser.me/api/portraits/men/62.jpg",
+  bio: "Hi, I'm John Doe. I'm a software engineer at Google. I love to code and learn new things. I'm a big fan of React and TypeScript. I also love to play guitar and read books.",
+  role: "Software Engineer",
+  socialLinks: {
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/",
+    github: "https://www.github.com/",
+  },
+  contactDetails: {
+    email: "hello@example.com",
+    phone: "+1 234 567 890",
+  },
+};
+export default user_data;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
+import user_data from "@/lib/data";
+
 const Home = () => {
   return (
-    <div>
-      Project Init
-    </div>
+    <section>
+      <div>
+        {/* profile image */}
+      </div>
+      <div>
+        {/* name */}
+      </div>
+      <div>
+        {/* bio */}
+      </div>
+    </section>
   );
 }
 
